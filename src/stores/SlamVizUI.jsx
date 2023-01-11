fd723c67108d28522382845dafb7ea6100d5e75d
Joel Tari <joel.tari31@gmail.com>
2023-01-12 00:41:11 +0100

    add README + store placeholder for the UI options

---
+++ src/stores/SlamVizUI.jsx
@@ -0,0 +1,11 @@
+// component not yet used
+
+import { createSignal } from "solid-js"
+
+const [SlamVizUI_opts, setSlamVizUI_opts] = createSignal(
+  {
+
+  }
+)
+
+export { SlamVizUI_opts, setSlamVizUI_opts }
