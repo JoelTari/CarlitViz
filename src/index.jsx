4341b4926d4a8b9db092049eff163e291c564aaa
Joel Tari <joel.tari31@gmail.com>
2023-01-11 17:25:08 +0100

    skeleton of the project (npm packages, components files, ...)

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,23 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+// render(() => <App />, document.getElementById('root'));
 
-render(() => <App />, document.getElementById('root'));
+import './style.css'
+
+// import the store
+import { setMixedFactorGraphData } from './stores/MixedFactorGraphData'
+// import the component
+import MixedFactorGraph from './MixedFactorGraph'
+// import fallback data
+import * as fallback_MixedFactorGraphData from "./stores/fallback_MixedFactorGraphData.json"
+
+
+// set store: if no data, use fallback data
+setMixedFactorGraphData( window.MixedFactorGraphData == null ?
+  fallback_MixedFactorGraphData : window.MixedFactorGraphData
+)
+
+render(() => <MixedFactorGraph />, document.getElementById('root'));
---
+++ src/stores/MixedFactorGraphData.jsx
@@ -0,0 +1,8 @@
+import { createSignal } from "solid-js"
+
+const [MixedFactorGraphData, setMixedFactorGraphData] = createSignal({})
+
+export {
+  MixedFactorGraphData,
+  setMixedFactorGraphData
+}
